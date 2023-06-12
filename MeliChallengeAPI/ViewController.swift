cdd25b0e84c8f9f59e6b6e7f6176d9ddcabbc409
Eduardo Hernandez <lalohernandez93@gmail.com>
2023-06-11 23:01:18 -0600

    Add all views to search storyboard and link them to controller

--- MeliChallengeAPI/ViewController.swift
+++ MeliChallengeAPI/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var btn_search: UIButton!
+    @IBOutlet weak var tf_search: UITextField!
+    
+    @IBOutlet weak var tableView_items: UITableView!
+    
+    @IBOutlet weak var img_emptyList: UIImageView!
+    @IBOutlet weak var lbl_emptyList: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemViewCell", for: indexPath)
+        return cell
+    }
+    
+    
 }
 
